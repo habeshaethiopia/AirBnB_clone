e39d09bc8dda4432f14711cd12befbaee2cb5a87
Adane Moges <adanemogs6@gmail.com>
2023-08-13 02:30:48 +0300

    console.py is update reached task 6

--- console.py
+++ console.py
@@ -3,23 +3,23 @@
 import cmd
 
 
-class console(cmd.Cmd):
-    """the AirBnB clone cmd"""
+class HBNBCommand(cmd.Cmd):
+    """the AirBnB clone cmd\n"""
     prompt = "(hbnb)"
 
     def do_EOF(self, arg):
-        """returns 0 and eixit the terminal"""
+        """returns 0 and eixit the terminal\n"""
         return True
 
     def do_quit(self, arg):
-        """You can use it to quit"""
+        """Quit command to exit the program\n"""
         return True
 
     def default(self, arg):
-        """the default statements"""
+        """the default statements\n"""
         self.onecmd(arg)
         return True
 
 
 if __name__ == '__main__':
-    console().cmdloop()
+   HBNBCommand().cmdloop()
