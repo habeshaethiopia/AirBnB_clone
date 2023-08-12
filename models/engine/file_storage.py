48e7ba448057658e431013a7c0ccdc459b79f611
Adane Moges <adanemogs6@gmail.com>
2023-08-13 02:21:45 +0300

    pycode style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,8 @@
 import json
 
 from models.base_model import BaseModel
+
+
 class FileStorage:
     """the file storage class"""
     __file_path = "models/engine/file.json"
@@ -11,6 +13,7 @@ class FileStorage:
     def all(self):
         """Return the dictionary of object"""
         return FileStorage.__objects
+
     def new(self, obj):
         """create anew object"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -19,17 +22,18 @@ class FileStorage:
     def save(self):
         """to json and save it to file"""
         saved_dict = {}
-        for key , obj in FileStorage.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             saved_dict[key] = obj.to_dict()
-        with open (FileStorage.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             file.write(json.dumps(saved_dict))
+
     def reload(self):
         deseri = {}
         """To relode from the json file"""
         try:
-            with open (self.__file_path) as file:
+            with open(self.__file_path) as file:
                 deseri = json.loads(file.read())
-            for key,value in deseri.items():
+            for key, value in deseri.items():
                 FileStorage.__objects[key] = BaseModel(value)
         except FileNotFoundError:
             pass
